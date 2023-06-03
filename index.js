6ae1e996911f95689c2c9ce29540cea1f0bda532
Travis <travis.williams94@gmail.com>
2023-06-03 14:16:00 -0600

    began building html and styling

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code here
+
+let addQuestion = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#addquestion");
+  const formContainer = document.querySelector("#formcontainer");
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form
+    addQuestion = !addQuestion;
+    if (addQuestion) {
+      formContainer.style.display = "block";
+    } else {
+      formContainer.style.display = "none";
+    }
+  });
+});
